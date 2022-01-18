3fdd739310adcb46b266f6cf58cb2f9cdc8ac7cd
yushi1999 <kontani1999@gmail.com>
2022-01-18 17:51:24 +0900

    Djangoフォームまで

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     # n件目の投稿がhttp://127.0.0.1:8000/post/{n}/に表示されるようにする
     # <int:pk>→整数値がpkという名前の変数でビューに渡されることを期待
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/',views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/',views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -14,3 +15,32 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == 'POST':
+        #methodがPOSTの場合(フォームにテキストが入力されて送信された時)
+        form=PostForm(request.POST)
+        if form.is_valid(): #必須フィールドの入力と不正な値が無いことの確認
+            #commit=False→まだauthorを指定していないのでPostモデルをまだ保存しない
+            post=form.save(commit=False)
+            post.author=request.user
+            post.published_date=timezone.now()
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form=PostForm()
+    return render(request,'blog/post_edit.html', {'form':form})
+
+def post_edit(request,pk):
+    post=get_object_or_404(Post, pk=pk)
+    if request.method=="POST":
+        form = PostForm(request.POST,instance=post)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author=request.user
+            post.published_date=timezone.now()
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form =PostForm(instance=post)
+    return render(request,'blog/post_edit.html',{'form':form})
