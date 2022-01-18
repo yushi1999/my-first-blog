af81d3b47ee4f5a00b8d555ca80d0b789ca3ec72
yushi1999 <kontani1999@gmail.com>
2022-01-18 16:31:57 +0900

    アプリケーションを拡張しよう まで

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # デフォルトのページ
     path('', views.post_list, name='post_list'),
+    # n件目の投稿がhttp://127.0.0.1:8000/post/{n}/に表示されるようにする
+    # <int:pk>→整数値がpkという名前の変数でビューに渡されることを期待
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
